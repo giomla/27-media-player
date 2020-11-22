f4b121110c9d96f71b27e673823c32b12b8615a7
Mladen <giomla93@gmail.com>
2020-11-22 07:35:16 +0100

    implementation of play, backwards, forwards, openfile, a dummy playlist, screen resolution, fixes

--- src/videoplayer.cpp
+++ src/videoplayer.cpp
@@ -8,6 +8,9 @@ videoplayer::videoplayer(QWidget *parent)
         m_mediaPlayer = new QMediaPlayer(this, QMediaPlayer::VideoSurface);
         const QRect screenGeometry = QApplication::desktop()->screenGeometry(this); // Pristupamo QDesktopWidget klasi koja ima metod
        // screenGeometry koji moze da nam vrati duzinu sirinu ekrana kao i broj ekrana.
+        m_playlist = new QMediaPlaylist();
+        m_mediaPlayer->setPlaylist(m_playlist);
+
         m_videoItem = new QGraphicsVideoItem;
         m_videoItem->setSize(QSizeF(screenGeometry.width()/3, screenGeometry.height()/2));
 
@@ -75,17 +78,25 @@ videoplayer::videoplayer(QWidget *parent)
         m_muteButton->setFixedSize(30,30);
 
         m_Slider = new QSlider(Qt::Horizontal);
-        m_Slider->setRange(0, 0);
+        //range of slider from zero to length of media played
+        m_Slider->setRange(0, m_mediaPlayer->duration()/1000);
+        m_Slider->setEnabled(false);
 
         QAbstractButton *openButton = new QPushButton(tr("Open"));
 
-        //Connection for open, play and mute buttons
-        // TODO - conncetions for stop,forward, backward
+        //Connection for different controls
+        connect(m_Slider, &QSlider::sliderMoved, this, &videoplayer::seek);
+        connect(m_mediaPlayer, &QMediaPlayer::durationChanged, this, &videoplayer::durationChanged);
+        connect(m_mediaPlayer, &QMediaPlayer::positionChanged, this, &videoplayer::positionChanged);
         connect(m_playButton,&QAbstractButton::clicked,this,&videoplayer::playClicked);
-        connect(openButton,&QAbstractButton::clicked,this,&videoplayer::openFile);
+        connect(openButton , &QAbstractButton::clicked,this,&videoplayer::openFile);
         connect(m_muteButton, &QAbstractButton::clicked, this, &videoplayer::muteClicked);
-
-
+        connect(m_forwardButton,&QAbstractButton::clicked, this, &videoplayer::forwardClicked);
+        connect(m_backwardButton,&QAbstractButton::clicked, this, &videoplayer::backwardClicked);
+        connect(m_stopButton,&QAbstractButton::clicked, this, &videoplayer::stopClicked);
+        //when another video is loaded up, the native resolution of the video changes
+        //so we monitor for that, so we can adjust the resolution accordingly
+        connect(m_videoItem, &QGraphicsVideoItem::nativeSizeChanged, this, &videoplayer::calcVideoFactor);
         QHBoxLayout* commandsLayout = new QHBoxLayout();
         commandsLayout->setMargin(0);
         commandsLayout->addWidget(m_backwardButton);
@@ -113,9 +124,20 @@ bool videoplayer::isAvaliable() const{
     return m_mediaPlayer->isAvailable();
 }
 
+void videoplayer::positionChanged(qint64 progress){
+    if (!m_Slider->isSliderDown())
+        m_Slider->setValue(progress/1000);
+}
+
 QMediaPlayer::State videoplayer::state() const
 {
     return m_playerState;
+}
+
+void videoplayer::durationChanged(qint64 duration)
+{
+    m_duration = duration / 1000;
+    m_Slider->setMaximum(m_duration);
 }
 
 void videoplayer::mediaStateChanged(QMediaPlayer::State state){
@@ -124,36 +146,64 @@ void videoplayer::mediaStateChanged(QMediaPlayer::State state){
 
         switch(state){
             case QMediaPlayer::PlayingState:
+                m_playButton->setEnabled(true);
+                m_muteButton->setEnabled(true);
+                m_forwardButton->setEnabled(true);
+                m_backwardButton->setEnabled(true);
                 m_stopButton->setEnabled(true);
+                m_Slider->setEnabled(true);
+                m_muteButton->setEnabled(true);
                 m_playButton->setIcon(style()->standardIcon(QStyle::SP_MediaPause));
                 break;
             case QMediaPlayer::StoppedState:
-                m_stopButton->setEnabled(false);
                 m_playButton->setIcon(style()->standardIcon(QStyle::SP_MediaPlay));
                 break;
             case QMediaPlayer::PausedState:
-                m_stopButton->setEnabled(true);
                 m_playButton->setIcon(style()->standardIcon(QStyle::SP_MediaPlay));
                 break;
             default:
-                 m_playButton->setIcon(style()->standardIcon(QStyle::SP_MediaPlay));
-                 break;
+                m_stopButton->setEnabled(false);
+                m_playButton->setIcon(style()->standardIcon(QStyle::SP_MediaPlay));
+                break;
         }
     }
 }
 
+//this is to avoid a known bug, so should work ok on all platforms, and not just some
+//https://bugreports.qt.io/browse/QTBUG-28850
+
+void videoplayer::calcVideoFactor(QSizeF size){
+     m_videoItem->setSize(QSizeF(size.width(),size.height()));
+}
+//TODO implementation of a true playlist with lists of urls and loading it up in the playlist
+//TODO aditional checks for supported files only to appear in file dialog
 void videoplayer::openFile(){
-    //TO DO
+    QFileDialog dialog(this);
+    dialog.setAcceptMode(QFileDialog::AcceptOpen);
+    dialog.setWindowTitle(tr("Open Files"));
+    dialog.setDirectory(QStandardPaths::standardLocations(QStandardPaths::MoviesLocation).value(0, QDir::homePath()));
+
+    if (dialog.exec() == QDialog::Accepted){
+        //we make a dummy playlist , with one video
+        //for development and testing purposes
+
+        m_playlist->addMedia(QFileDialog::getOpenFileUrl());
+        m_playlist->setCurrentIndex(1);
+        m_mediaPlayer->play();
+    }
 }
 
 void videoplayer::playClicked(){
     switch (m_playerState) {
         case QMediaPlayer::StoppedState:
         case QMediaPlayer::PausedState:
-            emit play();
+            //with emit doesnt work on Ubuntu
+            //emit play();
+            m_mediaPlayer->play();
             break;
         case QMediaPlayer::PlayingState:
-            emit pause();
+            //emit pause();
+            m_mediaPlayer->pause();
             break;
         }
 }
@@ -174,8 +224,50 @@ void videoplayer::setMuted(bool muted)
     }
 }
 
-void videoplayer::muteClicked(){
+void videoplayer::seek(int seconds)
+{
+    m_mediaPlayer->setPosition(seconds * 1000);
+}
+//for some reason, nothing with emit works on Ubuntu,
+//hence the commented out lines of code, might work just fine on other platforms
+void videoplayer::muteClicked()
+{
     emit changeMuting(!m_playerMuted);
 }
 
-//Ostale funckije potrebne pri konekciji
+void videoplayer::stopClicked()
+{
+    m_mediaPlayer->stop();
+    //emit stop();
+}
+//Not all playback services support change of the playback rate.
+//It is framework defined as to the status and quality of audio and video
+//while fast forwarding or rewinding.
+void videoplayer::forwardClicked()
+{
+    qreal value;
+    if (m_mediaPlayer->playbackRate()==0.01)
+        value = 0.0 + PLAYBACK_STEP;
+    else
+        value = m_mediaPlayer->playbackRate()+PLAYBACK_STEP;
+
+    if (value==0)
+        m_mediaPlayer->setPlaybackRate(0.01);
+    else if (value <= (MAX_PLAYBACK_RATE))
+        m_mediaPlayer->setPlaybackRate(value);
+}
+
+void videoplayer::backwardClicked()
+{
+    qreal value;
+    if (m_mediaPlayer->playbackRate()==0.01)
+        value = 0.0 + PLAYBACK_STEP;
+    else
+        value = m_mediaPlayer->playbackRate()-PLAYBACK_STEP;
+    //because we cant divide by zero the value of zero is not zero, so it can playback in reverse :D
+    //otherwise Gstreamer throws an exception
+    if (value==0)
+        m_mediaPlayer->setPlaybackRate(0.01);
+    else if (value >= (-MAX_PLAYBACK_RATE))
+        m_mediaPlayer->setPlaybackRate(value);
+}
